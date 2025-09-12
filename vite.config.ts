5c27f7b2d48250e532250248a7ac51e811217d17
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-09-13 00:11:23 +0200

    - force build before tests

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  //add tsconfigPaths() below
+  plugins: [tsconfigPaths({})],
+  test: {
+    environment: "node", // or "jsdom" if you're testing browser code
+  },
+});
